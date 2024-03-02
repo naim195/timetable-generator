20f3bb518ac54ad1dea1ae837442cfe07543a5c7
naim195 <119221365+naim195@users.noreply.github.com>
2024-03-02 06:08:56 +0530

    website functions properly

--- src/components/Timetable.jsx
+++ src/components/Timetable.jsx
@@ -6,35 +6,53 @@ import {
   TableRow,
   TableCell,
 } from "@mui/material";
-import data from "../timetable_slots.json";
+import timetableSlots from "../timetable_slots.json";
 
-const Timetable = (selectedCourses) => {
-  return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Time Slot</TableCell>
-          <TableCell>Monday</TableCell>
-          <TableCell>Tuesday</TableCell>
-          <TableCell>Wednesday</TableCell>
-          <TableCell>Thursday</TableCell>
-          <TableCell>Friday</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {data.map((slot, index) => (
-          <TableRow key={index}>
-            <TableCell>{slot.Slot}</TableCell>
-            <TableCell>{slot.M}</TableCell>
-            <TableCell>{slot.T}</TableCell>
-            <TableCell>{slot.W}</TableCell>
-            <TableCell>{slot.Th}</TableCell>
-            <TableCell>{slot.F}</TableCell>
+const Timetable = ({ selectedCourses }) => {
+    // Create a mapping from slot names to course IDs
+    const slotToCourse = {};
+    selectedCourses.forEach(course => {
+      course.Lecture.forEach(slot => {
+        slotToCourse[slot] = course["Course Code"];
+      });
+    });
+  
+    return (
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Time Slot</TableCell>
+            <TableCell>Monday</TableCell>
+            <TableCell>Tuesday</TableCell>
+            <TableCell>Wednesday</TableCell>
+            <TableCell>Thursday</TableCell>
+            <TableCell>Friday</TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
-};
-
+        </TableHead>
+        <TableBody>
+          {timetableSlots.map((slot, index) => (
+            <TableRow key={index}>
+              <TableCell>{slot.Slot}</TableCell>
+              <TableCell>
+                {slotToCourse[slot.M] || slot.M}
+              </TableCell>
+              <TableCell>
+                {slotToCourse[slot.T] || slot.T}
+              </TableCell>
+              <TableCell>
+                {slotToCourse[slot.W] || slot.W}
+              </TableCell>
+              <TableCell>
+                {slotToCourse[slot.Th] || slot.Th}
+              </TableCell>
+              <TableCell>
+                {slotToCourse[slot.F] || slot.F}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    );
+  };
+  
 export default Timetable;
